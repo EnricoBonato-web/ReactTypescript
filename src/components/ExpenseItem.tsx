c1de1f8d1e8433eb4ca7b2c98f4ca05b0e93ef51
Enrico <enrico.bonato95@gmail.com>
2022-05-11 10:49:40 +0200

    Lesson

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,4 +1,17 @@
-function ExpenseItem() {
-  return <h2> Expense item!</h2>;
-}
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props:{date:Date,title:string,amount:number}) {
+    return (
+      <div className='expense-item'>
+        <ExpenseDate date={props.date}></ExpenseDate> 
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </div>
+    );
+  }
+  
+
 export default ExpenseItem;
