af4db312721155f584127fc6dd8ccda25521cab9
Enrico <enrico.bonato95@gmail.com>
2022-05-11 16:49:17 +0200

    Lesson9

---
+++ src/components/Expenses.tsx
@@ -0,0 +1,16 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from './Card'
+
+function Expenses(items: { date: Date; title: string; amount: number }[]) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem {...items[0]} />
+      <ExpenseItem {...items[1]} />
+      <ExpenseItem {...items[2]} />
+      <ExpenseItem {...items[3]} />
+    </Card>
+  );
+}
+
+export default Expenses;
