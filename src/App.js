e0102138e6ae7a1c20bb9c023f2eb8beab7108df
Enrico <enrico.bonato95@gmail.com>
2022-05-11 17:04:28 +0200

    Spring Cleaning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseDiv from "./components/Expenses";
+import ExpenseDiv from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -29,6 +29,6 @@ function App() {
       <ExpenseDiv {...expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,6 +1,6 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props:{date:Date,title:string,amount:number}) {
     return (
@@ -10,6 +10,7 @@ function ExpenseItem(props:{date:Date,title:string,amount:number}) {
           <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button>Change Title</button>
       </Card>
     );
   }
