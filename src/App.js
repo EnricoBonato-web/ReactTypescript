8112d9424a78eb5c750299c4f013034e1a7c78f2
Enrico <enrico.bonato95@gmail.com>
2022-05-16 12:24:13 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import ExpenseDiv from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import ExpenseDiv from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+import ExpensesFilter from './components/Expenses/ExpenseFilter';
 
 const App = () => {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "New Desk (Wooden)",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
-
+  const addExpenseHandler = expenses => {
+    console.log(expenses);
+  };
   return (
     <div>
       <h2>Let's get started!</h2>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpensesFilter/>
       <ExpenseDiv {...expenses} />
     </div>
   );
 };
-
 export default App;
