b3db8ef0b7a1aa4687a1344af0f676da2da786ca
Enrico <enrico.bonato95@gmail.com>
2022-05-26 02:38:05 +0200

    CssModule

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -7,7 +7,11 @@ const CourseGoalList = props => {
   return (
     <ul className="goal-list">
       {props.items.map(goal => (
-        <CourseGoalItem key={goal.id} id={goal.id} onDelete={props.onDeleteItem}>
+        <CourseGoalItem
+          key={goal.id}
+          id={goal.id}
+          onDelete={props.onDeleteItem}
+        >
           {goal.text}
         </CourseGoalItem>
       ))}
