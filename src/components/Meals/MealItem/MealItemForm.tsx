52fdfbf7ca4afa5fd618a8d7c0f8995a0d5757d6
Enrico <enrico.bonato95@gmail.com>
2022-05-30 01:44:31 +0200

    137

---
+++ src/components/Meals/MealItem/MealItemForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+const MealItemForm = (props:any) => {
+return <form className={classes.form}>
+<input/>
+    <button>+ ADD</button>
+</form>
+
+};
+export default MealItemForm;
