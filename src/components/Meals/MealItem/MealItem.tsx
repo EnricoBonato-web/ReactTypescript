a6d550c400a77e705f7a3d99b86f7431c680fa4d
Enrico <enrico.bonato95@gmail.com>
2022-05-30 02:33:05 +0200

    138

--- src/components/Meals/MealItem/MealItem.tsx
+++ src/components/Meals/MealItem/MealItem.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
 
 const MealItems = (props: any) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,6 +12,7 @@ const MealItems = (props: any) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
+      <div><MealItemForm id={props.id}/></div>
     </li>
   );
 };
--- src/components/Meals/MealItem/MealItemForm.tsx
+++ src/components/Meals/MealItem/MealItemForm.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
-const MealItemForm = (props:any) => {
-return <form className={classes.form}>
-<input/>
-    <button>+ ADD</button>
-</form>
-
+import Input from "../../UI/Input";
+const MealItemForm = (props: any) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id, 
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ ADD</button>
+    </form>
+  );
 };
 export default MealItemForm;
