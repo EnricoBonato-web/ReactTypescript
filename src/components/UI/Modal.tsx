888a9684c6eefe084f09ba39a8ac5db8e91b2125
Enrico <enrico.bonato95@gmail.com>
2022-05-30 12:58:52 +0200

    w/o portal

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,4 +1,19 @@
+import React,{Fragment} from "react";
 import classes from "./Modal.module.css";
-
-const Modal = () => {};
+const Backdrop = (props: any) => {
+  return <div className={classes.backdrop}></div>;
+};
+const ModalOverlay = (props: any) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const Modal = (props: any) => {
+    return <Fragment>
+        <Backdrop/>
+        <ModalOverlay>{props.children}</ModalOverlay>
+    </Fragment>
+};
 export default Modal;
