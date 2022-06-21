aa7427d72fa6ff14ec903fb35d2bad4d1d10f8a7
Enrico <enrico.bonato95@gmail.com>
2022-06-21 18:38:07 +0200

    cose

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
-import classes from "./Counter.module.css";
 import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
